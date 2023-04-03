62f7d45273fd72b9ad46ecc9af9b1e39e1fb5ea4
Edward <andrushenkoedik3000@gmail.com>
2023-04-03 22:41:21 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use script';
+
+const category = 'toys';
+
+console.log(`http//someurl/com/$(category)/10`);
+
+let x = 5;
+
+console.log(x++);
+
+const Edik = true,
+	Alexander = false;
+
+console.log(Edik && Alexander);
